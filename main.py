d7f05d2d8e1a4a0f088b3f45215ff8b16dd610ad
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2024-12-07 16:49:07 +0100

    Update logging, add mt5 init

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import src.utils as utils
 
 def main():
     utils.init_logging()
+    utils.init_mt5()
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,23 @@
+import MetaTrader5 as mt5
 import logging
+import os
 
 def init_logging(name="main"):
     """
     Sets-up the logging library - save logs to a file
     """
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(filename=f"{name}.log", encoding='UTF-8', level=logging.INFO, filemode='w',
-                        format="%(asctime)s %(name)s %(levelname)s: %(message)s", datefmt='%I:%M:%S')
-    logger.info("Set-up logging successfully")
+    logging.basicConfig(encoding='UTF-8', level=logging.INFO,
+                       format="%(asctime)s %(name)s %(levelname)s: %(message)s", datefmt='%I:%M:%S',
+                       handlers=[logging.FileHandler(f"{name}.log", mode='w', encoding='UTF-8')])
+    logging.info("Set-up logging successfully")
+
+def init_mt5():
+    """
+    Connects to MetaTrader5
+    """
+    if not mt5.initialize():
+        error_code = mt5.last_error()
+        logging.error("MetaTrader5 initialization failed. Error code: %s", error_code)
+        return False
+    logging.info("Connected to MetaTrader5")
+    return True
